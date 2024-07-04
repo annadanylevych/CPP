e7194025bb74f5a411240dbbfc0c3efd29b08446
Anna Danylevych <adanylev@car7s3.42barcelona.com>
2024-07-04 17:01:44 +0200

    need to finish the second one

---
+++ module00/ex01/Contact.hpp
@@ -0,0 +1,36 @@
+#ifndef PHONEBOOK_HPP
+#define PHONEBOOK_HPP
+
+# include <iostream>
+# include <iomanip>
+# include <string>
+
+
+class   Contact
+{
+    private:
+        std::string number;
+        std::string firsName;
+        std::string lastName;
+        std::string nickName;
+        std::string secret;
+    public:
+        Contact()
+        {
+            number = NULL;
+            firsName = NULL;
+            lastName = NULL;
+            nickName = NULL;
+            secret = NULL;
+        }
+        
+
+
+}
+
+class   PhoneBook
+{
+    public:
+
+
+}
