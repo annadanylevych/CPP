25a2b9dc0d4c60a3426c5bdd279c9117abc8cce6
Anna Danylevych <adanylev@car11s4.42barcelona.com>
2024-10-21 14:46:09 +0200

    started

---
+++ module07/ex00/whatever.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <iostream>
+
+template <typename T> T max(T x, T y)
+{
+    return (x > y ? x : y);
+}
+
+template <typename T> T min (T x, T y)
+{
+    return (x < y ? x : y);
+}
+
+template <typename T> void swap(T& x, T& y)
+{
+    T aux = x;
+    x = y;
+    y = aux;
+}
---
+++ module07/ex01/iter.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <cstring>
+
+template <typename T> void iter(T* arr, size_t arr_len, ) 
